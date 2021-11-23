4e7bbcc5d42bc2169c8995ea7ea332b8f21e83ab
kkd <kkd@kkds-MacBook-Pro.local>
2021-11-22 22:58:05 -0600

    Add get_events_by_user

--- planeador_eventos_api/urls.py
+++ planeador_eventos_api/urls.py
@@ -1,16 +1,17 @@
 from django.urls import path
 
-from planeador_eventos_api.api.views.event import get_events, get_event, post_event, put_event, delete_event
+from planeador_eventos_api.api.views.event import get_events, get_events_by_user_uuid, get_event, post_event, put_event, delete_event
 from planeador_eventos_api.api.views.payment import get_payments, get_payment, post_payment, put_payment, delete_payment
 from planeador_eventos_api.api.views.product import get_products, get_product, post_product, put_product, delete_product
 from planeador_eventos_api.api.views.provider import get_providers, get_provider, get_provider_by_user_uuid, post_provider, put_provider, delete_provider
-from planeador_eventos_api.api.views.service import get_services, get_service, get_service_by_type, post_service, put_service, delete_service
+from planeador_eventos_api.api.views.service import get_services, get_service, get_services_by_type, post_service, put_service, delete_service
 from planeador_eventos_api.api.views.subscription import get_subscriptions, get_subscription, post_subscription, put_subscription, delete_subscription
 from planeador_eventos_api.api.views.user import get_users, get_user, get_user_by_email, post_user, put_user, delete_user
 
 urlpatterns = [
     path('api/planeador_eventos_api/events', get_events),
     path('api/planeador_eventos_api/event/<uuid:event_uuid>/', get_event),
+    path('api/planeador_eventos_api/events/user/<uuid:user_uuid>/', get_events_by_user_uuid),
     path('api/planeador_eventos_api/event/create/', post_event),
     path('api/planeador_eventos_api/event/update/<uuid:event_uuid>', put_event),
     path('api/planeador_eventos_api/event/delete/<uuid:event_uuid>', delete_event),
@@ -36,7 +37,7 @@ urlpatterns = [
 
     path('api/planeador_eventos_api/services', get_services),
     path('api/planeador_eventos_api/service/<uuid:service_uuid>', get_service),
-    path('api/planeador_eventos_api/service/type/<str:service_type>', get_service_by_type),
+    path('api/planeador_eventos_api/services/type/<str:service_type>', get_services_by_type),
     path('api/planeador_eventos_api/service/create/', post_service),
     path('api/planeador_eventos_api/service/update/<uuid:service_uuid>', put_service),
     path('api/planeador_eventos_api/service/delete/<uuid:service_uuid>', delete_service),
